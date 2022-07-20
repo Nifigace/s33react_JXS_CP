5e2d6cc17000b1f1b9a1e7211f9d636ee1d61513
Nifigace <53969580+Nifigace@users.noreply.github.com>
2022-07-20 14:45:59 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Title</h1>
+    <ul>
+      <li>111</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
