5294ea8f147b872ca18a0078638aa6c8c26d0c48
Nifigace <53969580+Nifigace@users.noreply.github.com>
2022-07-20 14:54:45 +0300

    got some practice

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "Roman";
+const num = Math.floor(Math.random() * 10);
+
 ReactDOM.render(
   <div>
-    <h1>Title</h1>
-    <ul>
-      <li>111</li>
-    </ul>
+    <h1>Lucky number</h1>
+    <p>
+      {name}, your lucky number is {num}
+    </p>
+    <p>if your name was Jack you'll have lucky number {num + 1}</p>
   </div>,
   document.getElementById("root")
 );
