2429628ec1a7601f0708d9aaf9dd55803b965ffe
Nifigace <53969580+Nifigace@users.noreply.github.com>
2022-07-20 15:00:20 +0300

    concotinated variables in code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Roman";
+const fName = "Roman";
+const lName = "Lisyuk";
 const num = Math.floor(Math.random() * 10);
 
 ReactDOM.render(
   <div>
-    <h1>Lucky number</h1>
+    <h1>Names</h1>
     <p>
-      {name}, your lucky number is {num}
+      Hello {`${fName} ${lName}`} your lucky number is {num}
     </p>
-    <p>if your name was Jack you'll have lucky number {num + 1}</p>
+    <p></p>
   </div>,
   document.getElementById("root")
 );
